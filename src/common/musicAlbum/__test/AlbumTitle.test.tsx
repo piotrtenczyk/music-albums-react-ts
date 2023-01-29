66f2249aad79e73b04a5497f1795292dbcdb4c03
Tomasz Sz <tomasz.szawara@gmail.com>
2023-01-29 11:08:51 +0100

    AlbumTitle.test.tsx completed

--- src/common/musicAlbum/__test/AlbumTitle.test.tsx
+++ src/common/musicAlbum/__test/AlbumTitle.test.tsx
@@ -1,5 +1,24 @@
+import { render, RenderResult } from "@testing-library/react";
+import exp from "constants";
+import AlbumTitle from "../AlbumTitle";
+
 describe("AlbumTitle.tsx", () => {
-  it("CO TO ZA KONKRETNY TEST", () => {
-    expect("ala ma kota").toEqual("ala ma psa");
+  it("matches snapshot for string children", () => {
+    const renderResult: RenderResult = render(
+      <AlbumTitle>My Greatest Hits</AlbumTitle>
+    );
+
+    const container = renderResult.container;
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("matches snapshot for a complex children elements", () => {
+    const { container } = render(
+      <AlbumTitle>
+        <div id="additional_div">text inside another div</div>
+      </AlbumTitle>
+    );
+    expect(container).toMatchSnapshot();
   });
 });
