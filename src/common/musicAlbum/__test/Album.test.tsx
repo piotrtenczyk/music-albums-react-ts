c4a57d84d4ef04cf42edee1825f97cf5a5bdece4
Tomasz Sz <tomasz.szawara@gmail.com>
2023-01-29 15:49:42 +0100

    trying to create 2nd test case for Album.tsx

--- src/common/musicAlbum/__test/Album.test.tsx
+++ src/common/musicAlbum/__test/Album.test.tsx
@@ -1,37 +1,47 @@
-import { render } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
 import Album from "../Album";
 import configureStore from "redux-mock-store";
 import { Provider } from "react-redux";
 
 const mockStore = configureStore();
 
-const initialState = {
-  shoppingCart: {
-    items: [],
-  },
+const imageDescription = {
+  title: "Męka przez testy",
+  artist: "DJ ZDFRONPol11",
+  price: 7,
+};
+
+const renderAlbumWithFakeStore = (): RenderResult => {
+  const initialState = {
+    shoppingCart: {
+      items: [],
+    },
+  };
+
+  const fakeStore = mockStore(initialState);
+  return render(
+    <Provider store={fakeStore}>
+      <Album
+        id="1"
+        number={1}
+        coverImageUrl="test-image-url"
+        description={imageDescription}
+      />
+    </Provider>
+  );
 };
 
 describe("Album.tsx", () => {
   it("matches snapshot", () => {
-    const fakeStore = mockStore(initialState);
-
-    const imageDescription = {
-      title: "Męka przez testy",
-      artist: "DJ ZDFRONPol11",
-      price: 7,
-    };
-
-    const { container } = render(
-      <Provider store={fakeStore}>
-        <Album
-          id="1"
-          number={1}
-          coverImageUrl="test-image-url"
-          description={imageDescription}
-        />
-      </Provider>
-    );
+    const { container } = renderAlbumWithFakeStore();
 
     expect(container).toMatchSnapshot();
+  });
+
+  it("shopping cart indicator is rendered when shopping cart contains album with same ID", () => {
+    // PYT 1:  Jak mozemy ustawic sytuacje gdzie jest juz jeden pasujacy album w state?
+    const { container } = renderAlbumWithFakeStore();
+
+    expect(true).toEqual(true); // PYT: 2 musimy wykombinowac jakis lepszy matcher :/
   });
 });
