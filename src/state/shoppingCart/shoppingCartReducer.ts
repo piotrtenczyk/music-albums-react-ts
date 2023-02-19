a01d01365df8a16c3c7052fddb1834501256c18a
Tomasz Sz <tomasz.szawara@gmail.com>
2023-02-19 13:25:41 +0100

    added redux and shoppingCartReducer

--- src/state/shoppingCart/shoppingCartReducer.ts
+++ src/state/shoppingCart/shoppingCartReducer.ts
@@ -2,17 +2,18 @@ import { ADD_ALBUM_TO_CART, ShoppingCartAction } from "./shoppingCartActions";
 
 interface ShoppingCartState {
   numberOfItems: number;
+  allItems: [];
 }
 
 const shoppingCartReducer = (
   state: ShoppingCartState,
   action: ShoppingCartAction
-) => {
+): ShoppingCartState => {
   switch (action.type) {
     case ADD_ALBUM_TO_CART:
-      const nowaIloscItemow = staraIlosc + 1;
-      return {};
-    //   return { ...state };
+      const newNumberOfItems = state.numberOfItems + 1;
+      const newState = { ...state, numberOfItems: newNumberOfItems };
+      return newState;
     default:
       throw new Error("Action dind't  match any known actions");
   }
