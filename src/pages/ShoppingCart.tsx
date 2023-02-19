c7dfad43e1a527ae42b4475a1da1aa51b73a0962
Tomasz Sz <tomasz.szawara@gmail.com>
2023-02-19 15:48:44 +0100

    new component SHoppingCart.tsx

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,4 +1,8 @@
+import { useAppSelector } from "../state/stateHooks";
+
 const ShoppingCart: React.FC = () => {
+  const shoppingCartItems = useAppSelector((state) => state.shoppingCart.items);
+
   return <>zsdzsds</>;
 };
 export default ShoppingCart;
