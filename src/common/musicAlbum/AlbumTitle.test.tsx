18f9f506bb0a1cbf300be39193da77738962080f
Tomasz Sz <tomasz.szawara@gmail.com>
2023-03-05 10:28:51 +0100

    AlbumTitle, written down test expectations

---
+++ src/common/musicAlbum/AlbumTitle.test.tsx
@@ -0,0 +1,11 @@
+describe("AlbumTitle.tsx", () => {
+  it("Nazwa mojego testu ... co dokladnie testje ????", () => {
+    // (1) Chcemy sprawdzic czy component <AlbumTitle /> sie wogole wyswietla
+    //  Pytanie 1: Jak uzywam AlbumTitle    Odp: <AlbumTitle>xxx</AlbumTitle>
+    //  Pytanie 2: Czego mam oczekiwac po wyswietleniu <AlbumTitle>xxx</AlbumTitle> ?
+    //    a) xxx
+    //    b) <p>xxx</p>
+    //    c) <div></div>
+    //    d) <div>xxx</div>
+  });
+});
