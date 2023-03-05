b7cada950ae92e0b7b07e3d42264596c8150d983
Tomasz Sz <tomasz.szawara@gmail.com>
2023-03-05 15:20:42 +0100

    date.test.ts with fake date

--- src/common/dates.test.ts
+++ src/common/dates.test.ts
@@ -4,6 +4,7 @@ describe("dates.ts", () => {
   it("daysAgo()", () => {
     jest.useFakeTimers();
     jest.setSystemTime(new Date("2023-03-05"));
+
     const date = new Date("2018-04-27");
     const daysAgoValue = daysAgo(date);
     expect(daysAgoValue).toEqual(1773);
