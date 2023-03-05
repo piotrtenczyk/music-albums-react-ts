23099a770233d923af0a1b9ca3be289ea02e5115
Tomasz Sz <tomasz.szawara@gmail.com>
2023-03-05 16:48:06 +0100

    optimized shoppingCartReducer.test

--- src/state/shoppingCart/shoppingCartReducer.test.ts
+++ src/state/shoppingCart/shoppingCartReducer.test.ts
@@ -2,75 +2,12 @@ import { ADD_ITEM_TO_CART } from "./shoppingCartActions";
 import shoppingCartReducer from "./shoppingCartReducer";
 
 describe("shoppingCartReducer.ts", () => {
-  it(`return appropiate state after ${ADD_ITEM_TO_CART} action`, () => {
-    const currentState = {
-      numberOfItems: 0,
-      items: [],
-    };
-
-    const newItem = {
-      id: "5673",
-      name: "Teddy Bear",
-      price: 123,
-      quantity: 1,
-    };
-
-    const addItemAction = {
-      type: ADD_ITEM_TO_CART as typeof ADD_ITEM_TO_CART,
-      item: newItem,
-    };
-
-    const newState = shoppingCartReducer(currentState, addItemAction);
-
-    //oczekiwany stan po teście
-    const expectedState = {
-      numberOfItems: 1,
-      items: [newItem],
-    };
-
-    expect(newState).toEqual(expectedState);
-  });
-
-  it(`return appropiate state after ${ADD_ITEM_TO_CART} action, when item with same id exists in state`, () => {
-    const item1 = {
-      id: "5673",
-      name: "Teddy Bear",
-      price: 123,
-      quantity: 1,
-    };
-
-    const currentState = {
-      numberOfItems: 1,
-      items: [item1],
-    };
-
-    const addItemAction = {
-      type: ADD_ITEM_TO_CART as typeof ADD_ITEM_TO_CART,
-      item: item1,
-    };
-
-    const newState = shoppingCartReducer(currentState, addItemAction);
-
-    //oczekiwany stan po teście
-    const expectedState = {
-      numberOfItems: 2,
-      items: [{ ...item1, quantity: 2 }],
-    };
-
-    expect(newState).toEqual(expectedState);
-  });
-
-  it(`return appropiate state after ${ADD_ITEM_TO_CART} action, when item with another id exists in state`, () => {
+  describe(`${ADD_ITEM_TO_CART} test`, () => {
     const teddyBear = {
       id: "5673",
       name: "Teddy Bear",
       price: 123,
       quantity: 1,
-    };
-
-    const currentState = {
-      numberOfItems: 1,
-      items: [teddyBear],
     };
 
     const coalaBear = {
@@ -80,19 +17,64 @@ describe("shoppingCartReducer.ts", () => {
       quantity: 1,
     };
 
-    const addItemAction = {
-      type: ADD_ITEM_TO_CART as typeof ADD_ITEM_TO_CART,
-      item: coalaBear,
+    const stateWithEmptyItems = {
+      numberOfItems: 0,
+      items: [],
     };
 
-    const newState = shoppingCartReducer(currentState, addItemAction);
-
-    //oczekiwany stan po teście
-    const expectedState = {
-      numberOfItems: 2,
-      items: [teddyBear, coalaBear],
+    const stateWithTeddyBear = {
+      ...stateWithEmptyItems,
+      numberOfItems: 1,
+      items: [teddyBear],
     };
 
-    expect(newState).toEqual(expectedState);
+    it(`return appropiate state after ${ADD_ITEM_TO_CART} action`, () => {
+      const addItemAction = {
+        type: ADD_ITEM_TO_CART as typeof ADD_ITEM_TO_CART,
+        item: teddyBear,
+      };
+
+      const newState = shoppingCartReducer(stateWithEmptyItems, addItemAction);
+
+      //oczekiwany stan po teście
+      const expectedState = {
+        numberOfItems: 1,
+        items: [teddyBear],
+      };
+
+      expect(newState).toEqual(expectedState);
+    });
+
+    it(`return appropiate state after ${ADD_ITEM_TO_CART} action, when item with same id exists in state`, () => {
+      const addItemAction = {
+        type: ADD_ITEM_TO_CART as typeof ADD_ITEM_TO_CART,
+        item: teddyBear,
+      };
+
+      const newState = shoppingCartReducer(stateWithTeddyBear, addItemAction);
+
+      const expectedState = {
+        numberOfItems: 2,
+        items: [{ ...teddyBear, quantity: 2 }],
+      };
+
+      expect(newState).toEqual(expectedState);
+    });
+
+    it(`return appropiate state after ${ADD_ITEM_TO_CART} action, when item with another id exists in state`, () => {
+      const addItemAction = {
+        type: ADD_ITEM_TO_CART as typeof ADD_ITEM_TO_CART,
+        item: coalaBear,
+      };
+
+      const newState = shoppingCartReducer(stateWithTeddyBear, addItemAction);
+
+      const expectedState = {
+        numberOfItems: 2,
+        items: [teddyBear, coalaBear],
+      };
+
+      expect(newState).toEqual(expectedState);
+    });
   });
 });
