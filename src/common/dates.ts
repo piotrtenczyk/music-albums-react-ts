bcf983ffd9a06efd0f57de062d61e48a3f5b1a94
Tomasz Sz <tomasz.szawara@gmail.com>
2023-03-05 14:06:52 +0100

    added dates.ts

---
+++ src/common/dates.ts
@@ -0,0 +1,9 @@
+export const daysAgo = (date: Date): number => {
+  const nowInMilliseconds: number = new Date().getTime();
+  const dateInMilliseconds: number = date.getTime();
+  const millisecondsSinceDate = nowInMilliseconds - dateInMilliseconds;
+  const secondsSinceDate = millisecondsSinceDate / 1000;
+  console.log(secondsSinceDate);
+  const daysSinceDate = secondsSinceDate / 60 / 60 / 24;
+  return Math.floor(daysSinceDate);
+};
