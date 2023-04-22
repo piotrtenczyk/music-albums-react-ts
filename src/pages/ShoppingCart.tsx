0553b1663350bce1f21ba199cbed50c8a3538d99
Tomasz Sz <tomasz.szawara@gmail.com>
2023-04-22 16:19:38 +0200

    remapped Album[] from state into ShoppingCartitem[]

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,10 +1,28 @@
 import ShoppingCartItems from "../common/ShoppingCartItems";
+import { useAppSelector } from "../state/store";
 
 const ShoppingCart: React.FC<{}> = () => {
+  const stateItems = useAppSelector((state) => state.items);
+
+  //   tablice obiektow:  {
+  //     title: string;
+  //     price: number;
+  //   }
+  //   mamy przeksztalcic na
+  //   tablice obiektow {
+  //     id: string;
+  //     label: string;
+  //     price: number;
+  //   }
+
+  const shoppingCartItems = stateItems.map((item, index) => {
+    return { id: index.toString(), price: item.price, label: item.title };
+  });
+
   return (
     <div>
       <div>your shopping cart</div>
-      <ShoppingCartItems items={[]} />
+      <ShoppingCartItems items={shoppingCartItems} />
     </div>
   );
 };
